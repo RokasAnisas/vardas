4099dd35b804bcd1d7c751730a5fcbde43ee7999
Mindaugas Čėsna <m.cesn4@gmail.com>
2020-11-29 19:07:12 +0200

    kilminko linksnio galunės

--- src/lib/linksnioKonvertavimas.ts
+++ src/lib/linksnioKonvertavimas.ts
@@ -1,4 +1,5 @@
 import { Linksniai } from '../types';
+import { kilmininkas } from './linksniuGalunes';
 
 const linksnioKonvertavimas = (vardas: string, linksnis: Linksniai): string => {
   return vardas + linksnis;
