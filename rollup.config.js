0b61f5483bfa82f44c4015a31da21b7beaabb085
Rokas <rokas.anisas@gmail.com>
2020-11-29 13:41:08 +0200

    initial build config

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript()],
+};
