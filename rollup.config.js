cc8979dbc0a0f04ec42f2a92d5d5f09b32de557d
Rokas <rokas.anisas@gmail.com>
2020-11-29 14:01:23 +0200

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,28 @@
 import typescript from 'rollup-plugin-typescript2';
+import copy from 'rollup-plugin-copy';
 import { terser } from 'rollup-plugin-terser';
+
+const packageJson = require('./package.json')
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.js',
+      file: `dist/${packageJson.main}`,
       format: 'cjs',
       sourcemap: true,
     },
     {
-      file: 'dist/index.esm.js',
+      file: `dist/${packageJson.module}`,
       format: 'esm',
       sourcemap: true,
     },
   ],
-  plugins: [typescript(), terser()],
+  plugins: [
+    copy({
+      targets: [{ src: 'package.json', dest: 'dist' }],
+    }),
+    typescript(),
+    terser(),
+  ],
 };
