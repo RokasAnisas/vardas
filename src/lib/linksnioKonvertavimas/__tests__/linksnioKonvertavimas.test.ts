aaf3b0eab29a6ff5a5766162ccd5c0f27b07ed17
Rokas <rokas.anisas@gmail.com>
2020-11-29 23:26:13 +0200

    Update linksnioKonvertavimas.test.ts

--- src/lib/linksnioKonvertavimas/__tests__/linksnioKonvertavimas.test.ts
+++ src/lib/linksnioKonvertavimas/__tests__/linksnioKonvertavimas.test.ts
@@ -1,11 +1,20 @@
 import linksnioKonvertavimas from '../linksnioKonvertavimas';
 
-describe('linksnioKonvertavimas', () => {
-  it('Turetu graziniti kilmininko linksni', () => {
-    const name = 'Rokas';
-    const expectedResult = 'Roko';
-    const result = linksnioKonvertavimas(name, 'kilmininkas');
 
-    expect(result).toBe(expectedResult);
+describe('linksnioKonvertavimas', () => {
+  const testVardai = ['Rokas', 'Mindaugas', 'Kamilė', 'Nerijus', 'Kąstytis', 'Ina', 'Saulius'];
+
+  it('Turėtų grąžinti kilmininko linksnyje', () => {
+    const expectedResult = ['Roko', 'Mindaugo', 'Kamilės', 'Nerijaus', 'Kąstyčio', 'Inos', 'Sauliaus'];
+    const result = testVardai.map(vardas => linksnioKonvertavimas(vardas, 'kilmininkas'));
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('Turėtų grąžinti naudininko linksnyje', () => {
+    const expectedResult = ['Rokui', 'Mindaugui', 'Kamilei', 'Nerijui', 'Kąstyčiui', 'Inai', 'Sauliui'];
+    const result = testVardai.map(vardas => linksnioKonvertavimas(vardas, 'naudininkas'));
+
+    expect(result).toEqual(expectedResult);
   });
 });
