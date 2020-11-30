d14cf027cbd8bc91e5af88e342473349e57b0c3d
Rokas <rokas.anisas@gmail.com>
2020-11-30 15:04:36 +0200

    refactored name testing

---
+++ src/lib/linksnioKonvertavimas/testiniaiVardai.ts
@@ -0,0 +1,120 @@
+export const testiniaiVardai: Array<TestVardai> = [
+  {
+    var: 'Rokas',
+    kil: 'Roko',
+    nau: 'Rokui',
+    gal: 'Roką',
+    įna: 'Roku',
+    vie: 'Roke',
+    šau: 'Rokai',
+  },
+  {
+    var: 'Mindaugas',
+    kil: 'Mindaugo',
+    nau: 'Mindaugui',
+    gal: 'Mindaugą',
+    įna: 'Mindaugu',
+    vie: 'Mindauge',
+    šau: 'Mindaugai',
+  },
+  {
+    var: 'Kamilė',
+    kil: 'Kamilės',
+    nau: 'Kamilei',
+    gal: 'Kamilę',
+    įna: 'Kamile',
+    vie: 'Kamilėje',
+    šau: 'Kamile',
+  },
+  {
+    var: 'Nerijus',
+    kil: 'Nerijaus',
+    nau: 'Nerijui',
+    gal: 'Nerijų',
+    įna: 'Nerijumi',
+    vie: 'Nerijuje',
+    šau: 'Nerijau',
+  },
+  {
+    var: 'Kąstytis',
+    kil: 'Kąstyčio',
+    nau: 'Kąstyčiui',
+    gal: 'Kąstytį',
+    įna: 'Kąstyčiu',
+    vie: 'Kąstytyje',
+    šau: 'Kąstyti',
+  },
+  {
+    var: 'Ina',
+    kil: 'Inos',
+    nau: 'Inai',
+    gal: 'Iną',
+    įna: 'Ina',
+    vie: 'Inoje',
+    šau: 'Ina',
+  },
+  {
+    var: 'Alfreda',
+    kil: 'Alfredos',
+    nau: 'Alfredai',
+    gal: 'Alfredą',
+    įna: 'Alfreda',
+    vie: 'Alfredoje',
+    šau: 'Alfreda',
+  },
+  {
+    var: 'Ainis',
+    kil: 'Ainio',
+    nau: 'Ainiui',
+    gal: 'Ainį',
+    įna: 'Ainiu',
+    vie: 'Ainyje',
+    šau: 'Aini',
+  },
+  {
+    var: 'Valys',
+    kil: 'Valio',
+    nau: 'Valiui',
+    gal: 'Valį',
+    įna: 'Valiu',
+    vie: 'Valyje',
+    šau: 'Valy',
+  },
+  {
+    var: 'Neivydis',
+    kil: 'Neivydžio',
+    nau: 'Neivydžiui',
+    gal: 'Neivydį',
+    įna: 'Neivydžiu',
+    vie: 'Neivydyje',
+    šau: 'Neivydi',
+  },
+  {
+    var: 'Vacys',
+    kil: 'Vacio',
+    nau: 'Vaciui',
+    gal: 'Vacį',
+    įna: 'Vaciu',
+    vie: 'Vacyje',
+    šau: 'Vacy',
+  },
+  {
+    var: 'Domantas',
+    kil: 'Domanto',
+    nau: 'Domantui',
+    gal: 'Domantą',
+    įna: 'Domantu',
+    vie: 'Domante',
+    šau: 'Domantai',
+  }
+];
+
+interface TestVardai {
+  var: string;
+  kil: string;
+  nau: string;
+  gal: string;
+  įna: string;
+  vie: string;
+  šau: string;
+}
